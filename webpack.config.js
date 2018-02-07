688d79a9cf2ed00e605276869e066d82d6e8f583
Ricky Do <ricky.dop@gmail.com>
2018-02-07 16:28:25 -0600

    added hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+
 module.exports = {
+    entry: './src/client.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
     devServer: {
-        // inline: true, 
+        inline: true, 
         contentBase:'./public',
         port:3000
     }
