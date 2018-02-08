51beb686c4a17abf8fc4e98088d646fe3d057b8b
Ricky Do <ricky.dop@gmail.com>
2018-02-08 16:45:16 -0600

    adding in transitions

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 
 class App extends React.Component {
     render(){
         return (
-            <Jumbotron className="-fluid text-md-center custom" containerFluid={true}>
-                    <h1>Building React user interfaces </h1>
-                    <p>with bootstrap and SASS</p>
-                    <p>
-                        <Button className="-primary" href="http://exceedingstatusdo.com" target="_blank" disabled/>
-                        <Button className="-outline-danger -sm" disabled/>
-                    </p>
-                    <Button className="-outline-primary -lg -block" disabled/>
-            </Jumbotron>
+            <ReactCSSTransitionGroup transitionName='app' transitionAppear={true} 
+                transitionAppearTimeout={500} transitionEnterTimeout={500}
+                transitionLeaveTimeout={500}>
+                <Jumbotron className="-fluid custom" containerFluid={true}>
+                        <h1>Building React user interfaces </h1>
+                        <p>with bootstrap and SASS</p>
+                        <p>
+                            <Button className="-primary" href="http://exceedingstatusdo.com" target="_blank" disabled/>
+                            <Button className="-outline-danger -sm" disabled/>
+                        </p>
+                        <Button className="-outline-primary -lg -block" disabled/>
+                </Jumbotron>
+            </ReactCSSTransitionGroup>
         )
     }
 }
