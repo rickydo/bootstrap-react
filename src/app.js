5cd4e4021381d49a70c329c65ccc8ce08dddf67f
Ricky Do <ricky.dop@gmail.com>
2018-02-08 16:58:09 -0600

    adding constructor to component

--- src/app.js
+++ src/app.js
@@ -5,6 +5,21 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 
 class App extends React.Component {
+    // function called when component is being created
+    // constructor will fetch props and give it to Component
+    constructor(props) {
+        // go to my extended parent and send to its constructor props
+        super(props);
+
+        // binding of this will now pass 'this' to onClick
+        // otherwise onclick will return undefined
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(e){
+        console.log(this);
+    }
+
     render(){
         return (
             <ReactCSSTransitionGroup transitionName='app' transitionAppear={true} 
@@ -17,7 +32,7 @@ class App extends React.Component {
                             <Button className="-primary" href="http://exceedingstatusdo.com" target="_blank" label="Button 1"/>
                             <Button className="-danger -sm" label="Button 2"/>
                         </p>
-                        <Button className="-primary -lg -block" label="Button 3"/>
+                        <Button onClick={this.onClick} className="-primary -lg -block" label="Button 3"/>
                 </Jumbotron>
             </ReactCSSTransitionGroup>
         )
