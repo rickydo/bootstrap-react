7f98186ad4b43ae188dca4e9df19463f039e76f4
Ricky Do <ricky.dop@gmail.com>
2018-02-08 15:12:30 -0600

    stretch jumbotron image to cover

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import Jumbotron from './bootstrap/jumbotron';
 class App extends React.Component {
     render(){
         return (
-            <Jumbotron className="-fluid text-md-center" style={{backgroundImage: 'url(img/tree-3097419_960_720.jpg', color:"white"}} containerFluid={true}>
+            <Jumbotron className="-fluid text-md-center" style={{backgroundImage: 'url(img/tree-3097419_960_720.jpg', color:"white", backgroundSize:"cover"}} containerFluid={true}>
                     <h1>Building React user interfaces </h1>
                     <p>with bootstrap and SASS</p>
                     <p>
