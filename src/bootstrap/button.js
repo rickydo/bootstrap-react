93b6825b22ac3c2994a462970e1cc31cdb01ac7c
Ricky Do <ricky.dop@gmail.com>
2018-02-08 12:35:59 -0600

    refactor out utils from button

--- src/bootstrap/button.js
+++ src/bootstrap/button.js
@@ -1,19 +1,11 @@
 import React from 'react';
+import {classify} from './utils';
 
 export default class Button extends React.Component {
-    // add btn to classes
-    classify(key, value){
-        if(value[0]==='-') value = key+value;
-
-        // use string literals
-        value = value.split(' -').join(` ${key}-`)
-        return `${key} ${value}`;
-    }
-
     render() {
         // uppercase Tag for dynamic, has to be uppercase
         const Tag = this.props.href? 'a':'button';
-        const className = this.classify('btn', this.props.className + (this.props.disabled? ' disabled':''));
+        const className = classify('btn', this.props.className + (this.props.disabled? ' disabled':''));
         
         return (
             // <a className={this.props.className} href={this.props.href} target={this.props.target}>My Website</a>
---
+++ src/bootstrap/utils.js
@@ -0,0 +1,8 @@
+// add btn to classes
+export function classify(key, value){
+    if(value[0]==='-') value = key+value;
+
+    // use string literals
+    value = value.split(' -').join(` ${key}-`)
+    return `${key} ${value}`;
+}
