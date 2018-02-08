73bf2525efac187d8a30055bcd53be445ca505c0
Ricky Do <ricky.dop@gmail.com>
2018-02-08 11:27:55 -0600

    extract app component from client.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-import { React, Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
     render(){
-        return <div className="jumbotron jumbotron-fluid text-md-center" style={{backgroundImage: 'url(img/tree-3097419_960_720.jpg', color:"white"}}>
+        return (<div className="jumbotron jumbotron-fluid text-md-center" style={{backgroundImage: 'url(img/tree-3097419_960_720.jpg', color:"white"}}>
         <div className="container">
             <h1>Building React user interfaces </h1>
             <p>with bootstrap and SASS</p>
             <p><a className="btn btn-primary" href="http://exceedingstatusdo.com" target="_blank">My Website</a></p>
         </div>
-    </div>
+    </div>)
     }
 }
 
