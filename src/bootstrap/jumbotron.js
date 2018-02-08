17648569851aa0d30f7bd92e485c9bacee5aa531
Ricky Do <ricky.dop@gmail.com>
2018-02-08 15:02:50 -0600

    handle errors if users pass in unwanted props

--- src/bootstrap/jumbotron.js
+++ src/bootstrap/jumbotron.js
@@ -4,10 +4,19 @@ import Container from "./container";
 
 export default class Jumbotron extends React.Component {
     render(){
+        // assign into this object all the properties that are enumerable 
+        // inside of this.props
+        const props = Object.assign({}, this.props);
+        const containerFluid = props.containerFluid;
+
+        // we can't delete the var containerFluid itself because it's constant
+        // but we can delete the props 
+        delete props.containerFluid;
+
         return (
-            <div {...this.props} className={classify('jumbotron', this.props.className)} >
+            <div {...props} className={classify('jumbotron', props.className)} >
                 {/* style here needs two pairs of curly brackets */}
-                <Container>
+                <Container fluid={containerFluid}>
                     {/* no content is actually displayed without the bottom line */}
                     {this.props.children}
                 </Container>
